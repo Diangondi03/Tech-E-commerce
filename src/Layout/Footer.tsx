6ff694e4b15166504d93e54be2317aed19343ca4
Guito22 <elguito2003@gmail.com>
2025-01-29 20:45:10 -0400

    Fix: Footer is now responsive and upper part replaced with accordion when display is under md

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,32 +1,48 @@
-import { Button } from "@heroui/react";
+import { Accordion, AccordionItem, Button } from "@heroui/react";
 import { FaTiktok,FaTwitter,FaFacebook,FaInstagram,FaYoutube } from "react-icons/fa";
+
 const socialMediaIcons = [FaFacebook,FaTwitter,FaInstagram,FaYoutube,FaTiktok]
+
+const additionalInfo = [
+    {title:"Support", links:["Contact Us","FAQ","Shipping"]},
+    {title:"About Us", links:["About Techtopia","Careers","Suppliers"]},
+    {title:"Customer Service", links:["Support","FAQ","Contact Us"]}
+]
 
 const Footer = () => {
   return (
     <>
-    <div className="w-full py-8 bg-gray-600 text-white text-center">
+    <div className="hidden md:flex w-full py-8 bg-gray-600 text-white text-center">
         <div className="container mx-auto flex flex-col md:flex-row justify-around">
-            <div className="mb-4 md:mb-0">
-            <h2 className="text-lg font-semibold mb-2">About Us</h2>
-            <ul>
-                <li><a className="hover:underline cursor-pointer">About Techtopia</a></li>
-                <li><a className="hover:underline cursor-pointer">Careers</a></li>
-                <li><a className="hover:underline cursor-pointer">Suppliers</a></li>
-            </ul>
-            </div>
-            <div>
-            <h2 className="text-lg font-semibold mb-2">Customer Service</h2>
-            <ul>
-                <li><a className="hover:underline cursor-pointer">Support</a></li>
-                <li><a className="hover:underline cursor-pointer">FAQ</a></li>
-                <li><a className="hover:underline cursor-pointer">Contact Us</a></li>
-            </ul>
-            </div>
+            {additionalInfo.map((info, index) => (
+                <div key={index} className="flex flex-col gap-2">
+                    <h3 className="font-bold">{info.title}</h3>
+                    {info.links.map((link, index) => (
+                        <a key={index} className="hover:underline cursor-pointer">{link}</a>
+                    ))}
+                </div>
+            ))}
         </div>
     </div>
 
-    <div className="w-full py-4 bg-gray-800 text-white text-center flex items-center justify-around">
+    <Accordion className="md:hidden container mx-auto py-4 cursor:pointer">
+        {additionalInfo.map((info, index) => (
+            <AccordionItem 
+            key={index} 
+            title={info.title} 
+            className="font-semibold cursor-pointer">
+                <div className="flex flex-col gap-2">
+                    {info.links.map((link, index) => (
+                        <a key={index} className="hover:underline cursor-pointer ml-4">{link}</a>
+                    ))}
+                </div>
+            </AccordionItem>
+
+        ))}
+
+    </Accordion>
+
+    <div className="w-full py-4 bg-gray-800 text-white text-center flex flex-col md:flex-row items-center justify-around gap-4">
         <div className="flex flex-row gap-4">
             {socialMediaIcons.map((Icon,index)=>(
                 <Button key={index} isIconOnly className="rounded-full bg-gray-700 cursor-pointer">
